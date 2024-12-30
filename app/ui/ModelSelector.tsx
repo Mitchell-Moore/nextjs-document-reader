2fbe7f917de68b64cd03148680b96ed5d16af12f
Mitch <mitchell2077@gmail.com>
2024-12-30 11:43:54 -0400

    update model selector to add cheveron svg

--- app/ui/ModelSelector.tsx
+++ app/ui/ModelSelector.tsx
@@ -33,17 +33,36 @@ export default function ModelSelector(props: {
 
   return (
     <div className="relative" ref={containerRef}>
-      <input
-        className="flex h-10 items-center justify-between rounded-md border bg-background px-3 py-2 text-sm  placeholder:text-gray-600 disabled:cursor-not-allowed disabled:opacity-50 w-[180px] cursor-pointer"
-        type="text"
-        readOnly
-        value={currentValue.label}
-        onFocus={() => {
-          setIsOpen(true);
+      <button
+        type="button"
+        role="combobox"
+        aria-controls="radix-:r2:"
+        aria-expanded="false"
+        aria-autocomplete="none"
+        dir="ltr"
+        data-state="closed"
+        className="flex h-10 items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm  placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 [&amp;>span]:line-clamp-1 w-[180px]"
+        onClick={() => {
+          setIsOpen(!isOpen);
         }}
-        name="model-selector"
-        id="model-selector"
-      />
+      >
+        <span>{currentValue.label}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-chevron-down h-4 w-4 opacity-50"
+          aria-hidden="true"
+        >
+          <path d="m6 9 6 6 6-6"></path>
+        </svg>
+      </button>
       {isOpen && (
         <div className="absolute top-full mt-1 left-0 right-0 w-full bg-white border border-gray-300 rounded-md shadow-md z-10">
           {props.models.map((model) => (
